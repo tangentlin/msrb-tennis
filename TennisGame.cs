67513af79bf18708d8a7df71073a945f4c6c8660
Tianzhen Lin <tangent@usa.net>
2017-11-17 09:56:51 -0500

    Add editor config and reformat code

--- TennisGame.cs
+++ TennisGame.cs
@@ -9,13 +9,13 @@ namespace Tennis
     private string player1Name;
     private string player2Name;
 
-    public TennisGame (string player1Name, string player2Name)
+    public TennisGame(string player1Name, string player2Name)
     {
       this.player1Name = player1Name;
       this.player2Name = player2Name;
     }
 
-    public void WonPoint (string playerName)
+    public void WonPoint(string playerName)
     {
       if (playerName == "player1")
         m_score1 += 1;
@@ -23,58 +23,59 @@ namespace Tennis
         m_score2 += 1;
     }
 
-    public string GetScore ()
+    public string GetScore()
     {
       String score = "";
-      int tempScore=0;
-      if (m_score1==m_score2)
+      int tempScore = 0;
+      if (m_score1 == m_score2)
       {
         switch (m_score1)
         {
-        case 0:
-          score = "Love-All";
-          break;
-        case 1:
-          score = "Fifteen-All";
-          break;
-        case 2:
-          score = "Thirty-All";
-          break;
+          case 0:
+            score = "Love-All";
+            break;
+          case 1:
+            score = "Fifteen-All";
+            break;
+          case 2:
+            score = "Thirty-All";
+            break;
         }
       }
-      else if (m_score1>=4 || m_score2>=4)
+      else if (m_score1 >= 4 || m_score2 >= 4)
       {
-        int minusResult = m_score1-m_score2;
-		if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
+        int minusResult = m_score1 - m_score2;
+        if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
       }
       else
       {
-        for (int i=1; i<3; i++)
+        for (int i = 1; i < 3; i++)
         {
-          if (i==1) tempScore = m_score1;
-          else { score+="-"; tempScore = m_score2;}
-          switch(tempScore)
+          if (i == 1) tempScore = m_score1;
+          else
           {
-          case 0:
-            score+="Love";
-            break;
-          case 1:
-            score+="Fifteen";
-            break;
-          case 2:
-            score+="Thirty";
-            break;
-          case 3:
-            score+="Forty";
-            break;
+            score += "-";
+            tempScore = m_score2;
+          }
+          switch (tempScore)
+          {
+            case 0:
+              score += "Love";
+              break;
+            case 1:
+              score += "Fifteen";
+              break;
+            case 2:
+              score += "Thirty";
+              break;
+            case 3:
+              score += "Forty";
+              break;
           }
         }
       }
       return score;
     }
   }
-
 }
-
-
